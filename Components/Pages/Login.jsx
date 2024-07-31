347c80476efdc0f47cec0fb493aef65b9cfad6b6
MwangiDavies2002 <86705902+MwangiDavies2002@users.noreply.github.com>
2024-07-31 03:39:39 +0300

    Add files via upload

---
+++ Components/Pages/Login.jsx
@@ -0,0 +1,15 @@
+import Header from "../Components/Header.js"
+import Login from "../Components/Login.js"
+export default function LoginPage(){
+    return(
+        <>
+             <Header
+                heading="Login to your account"
+                paragraph="Don't have an account yet? "
+                linkName="Signup"
+                linkUrl="/signup"
+                />
+            <Login/>
+        </>
+    )
+}
